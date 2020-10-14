5fa421803e428298d0492df70f5126c928b2afb4
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-14 10:45:33 +0530

    welcome to user registrtion added email

--- userRegistrtion.sh
+++ userRegistrtion.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 echo "welcome to user registration"
+##email validation
+read -p "enter email please: " email 
+pattern="^[A-Za-z0-9]+([._+-][0-9a-zA-Z]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?"
+if [[ $email =~ $pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
